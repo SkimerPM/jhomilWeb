709a83a50f96f945678f3e060ee303cdd964ab09
SkimerPM <jose.sanchez.q@tecsup.edu.pe>
2025-10-16 16:34:31 -0500

    first commit

---
+++ src/main/java/com/jhomilmotors/jhomilwebapp/JhomilWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.jhomilmotors.jhomilwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhomilWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhomilWebAppApplication.class, args);
+    }
+
+}
