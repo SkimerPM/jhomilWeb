6d821509e1061d6c8ba4b479d42cc044948ee4f0
Felix Díaz <felix.diaz.b@tecsup.edu.pe>
2025-11-29 17:15:45 -0500

    Impl. de google para autenticacion en Android

---
+++ src/main/java/com/jhomilmotors/jhomilwebapp/dto/GoogleTokenDto.java
@@ -0,0 +1,9 @@
+package com.jhomilmotors.jhomilwebapp.dto;
+
+public class GoogleTokenDto {
+    private String idToken;
+
+    // Getters y Setters necesarios
+    public String getIdToken() { return idToken; }
+    public void setIdToken(String idToken) { this.idToken = idToken; }
+}
